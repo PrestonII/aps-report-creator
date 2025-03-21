22015ab85395c21d9c13ec85fb2743ac1aa27d3d
Preston Smith <smith.preston2@gmail.com>
2025-03-21 19:30:22 -0400

    remove delete walls command

--- Design-Automation_Version/ReportCreatorApp/CreateReportsApp.cs
+++ Design-Automation_Version/ReportCreatorApp/CreateReportsApp.cs
@@ -30,31 +30,6 @@ namespace ipx.revit.reports
          ExportToPdfs(e.DesignAutomationData);
       }
 
-      public static void DeleteAllWalls(DesignAutomationData data)
-      {
-         if (data == null) throw new ArgumentNullException(nameof(data));
-
-         Application rvtApp = data.RevitApp;
-         if (rvtApp == null) throw new InvalidDataException(nameof(rvtApp));
-
-         string modelPath = data.FilePath;
-         if (String.IsNullOrWhiteSpace(modelPath)) throw new InvalidDataException(nameof(modelPath));
-
-         Document doc = data.RevitDoc;
-         if (doc == null) throw new InvalidOperationException("Could not open document.");
-
-         using (Transaction transaction = new Transaction(doc))
-         {
-            FilteredElementCollector col = new FilteredElementCollector(doc).OfClass(typeof(Wall));
-            transaction.Start("Delete All Walls");
-            doc.Delete(col.ToElementIds());
-            transaction.Commit();
-         }
-
-         ModelPath path = ModelPathUtils.ConvertUserVisiblePathToModelPath("result.rvt");
-         doc.SaveAs(path, new SaveAsOptions());
-      }
-
     public bool ExportToPdfs(DesignAutomationData data)
     {
       if (data == null)
@@ -116,7 +91,7 @@ namespace ipx.revit.reports
         if (0 < views.Count)
         {
           PDFExportOptions options = new PDFExportOptions();
-          options.FileName = "result";
+          options.FileName = "result"; 
           options.Combine = true;
           string workingFolder = Directory.GetCurrentDirectory();
           doc.Export(workingFolder, viewIds, options);
